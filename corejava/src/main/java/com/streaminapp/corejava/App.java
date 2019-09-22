874ca1ae351a4132ff591ab6b30c11643dcdce27
abhishek14june <abhishek.ras14@gmail.com>
2019-09-22 15:04:40 +0530

    from STS

--- corejava/src/main/java/com/streaminapp/corejava/App.java
+++ corejava/src/main/java/com/streaminapp/corejava/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         
         System.out.println("hello from juniorGeek");
+        System.out.println("dmslkjdlksjdklsjd");
     }
 }
