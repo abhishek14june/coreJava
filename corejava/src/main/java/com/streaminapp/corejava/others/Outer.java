da7693fa7c10bf28ca65affe00815380f3dfdf8f
abhishek14june <abhishek.ras14@gmail.com>
2019-09-25 15:43:54 +0530

    24-09-2019

---
+++ corejava/src/main/java/com/streaminapp/corejava/others/Outer.java
@@ -0,0 +1,28 @@
+package com.streaminapp.corejava.others;
+
+import com.streaminapp.corejava.others.Outer.Inner;
+
+
+public class Outer{
+	private void m() {
+		System.out.println("Outer.m()");
+	}
+	
+	class Inner extends Outer {
+		private void m() {
+			System.out.println("Inner.m()");
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Outer outerObj=new Outer();
+		Inner innerObj=outerObj.new Inner();
+		innerObj.m();
+		
+		outerObj=innerObj;
+		
+		outerObj.m();
+		
+	}
+}
--- corejava/src/main/java/com/streaminapp/corejava/others/StaticShadow.java
+++ corejava/src/main/java/com/streaminapp/corejava/others/StaticShadow.java
@@ -3,14 +3,10 @@ package com.streaminapp.corejava.others;
 public class StaticShadow {
 public static void main(String[] args) {
 	A obj=new B();
-	//static method of a class cannot be overriden in child class
-	// static method is class property so even if we invoke the static method show()
-	//from object of child class ... method of class A will be called
+
+	
 	
 	obj.show();
-	
-	
-	
 	obj.show2();
 	
 	
@@ -18,7 +14,7 @@ public static void main(String[] args) {
 }
 }
 class A{
-	static void show() {
+	 void show() {
 		System.out.println("A.show()");
 	}
 	 void show2() {
@@ -27,11 +23,11 @@ class A{
 }
 class B extends A{
 	
-	static void show() {
+	 void show() {
 		System.out.println("B.show()");
 	}
 	
-	@Override
+	
 	void show2() {
 		System.out.println("B.show2()");
 	}
